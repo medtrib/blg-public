abcff966f26336ca20af51d1525bdd3d1344e835
sulwan <medtrib@gmail.com>
2024-10-29 09:53:36 +0800

    增加用户模块API

---
+++ cmd/blg-public/wire.go
@@ -0,0 +1,17 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(newApp))
+}
